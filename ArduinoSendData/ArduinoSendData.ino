25ee133c9018b17f4cdc20ba27378cf7a31f35e7
Shyheim Russell <shyheim.russell@students.olin.edu>
2018-09-25 03:18:44 -0400

    Arduino code with for loops

--- ArduinoSendData/ArduinoSendData.ino
+++ ArduinoSendData/ArduinoSendData.ino
@@ -14,7 +14,8 @@ const int servoPin = 3;
 const int servoPin2= 10;
 
 int sensorVal = 0;                          // variable to storw the value coming from the sensor, set to 0 initially
-int pos = 180;
+int pos = 0;
+int pos1 = 0;
 int value = 0;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -36,27 +37,55 @@ void loop()
 {  
    /// Infrared Distance Sensor Calibration code
 
-    value = analogRead(SENSOR);
-    Serial.println(value);
-    delay(100);
+//    value = analogRead(SENSOR);
+//    Serial.println(value);
+//    delay(100);
     
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
   ///Servo1 Movement on its own 
   
-   for(pos = 0; pos < 180; pos += 1)           // goes from 0 degrees to 180 degrees
+   for(pos = 0; pos < 100; pos += 5)           // goes from 0 degrees to 180 degrees
   {                                            // in steps of 1 degree
     Servo1.write(pos);                         // tell servo to go to position in variable 'pos'
-    delay(15);                                 // waits 15ms for the servo to reach the position
-    Servo2.write(pos);
-    delay(15);
+    delay(100);                                 // waits 15ms for the servo to reach the position
+    for(pos1 = 0; pos1 < 100; pos1 += 1)           // goes from 0 degrees to 180 degrees
+  
+  {
+    Servo2.write(pos1);
+//    delay(15);
+//    Servo1.write(pos1);                          // tell servo to go to position in variable 'pos'
+////    delay(100);                                  // waits 15ms for the servo to reach the position
+    value = analogRead(SENSOR);
+    Serial.println(pos);
+    Serial.print(';');
+    Serial.print(pos1);
+    Serial.print(';');
+    Serial.print(value);
+    Serial.print(';');
+    delay(50);
   }
-  for(pos = 180; pos>=1; pos-=1)                // goes from 180 degrees to 0 degrees
+  }
+  for(pos = 100; pos>=1; pos-=5);                // goes from 180 degrees to 0 degrees
   {                               
     Servo1.write(pos);                          // tell servo to go to position in variable 'pos'
-    delay(15);                                  // waits 15ms for the servo to reach the position
-    Servo2.write(pos);
-    delay(15);
+    delay(100);                                  // waits 15ms for the servo to reach the position
+    for(pos1 = 100; pos1>=1; pos1-=1)                // goes from 180 degrees to 0 degrees
+  { 
+    Servo2.write(pos1);
+//    delay(15);
+//    Servo1.write(pos1);                          // tell servo to go to position in variable 'pos'
+////    delay(100);                                  // waits 15ms for the servo to reach the position
+    value = analogRead(SENSOR);
+    Serial.println(pos);
+    Serial.print(';');
+    Serial.print(pos1);
+    Serial.print(';');
+    Serial.print(value);
+    Serial.print(';');
+    delay(50);
+  }
+  
   }}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
